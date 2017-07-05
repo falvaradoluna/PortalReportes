6a3f1e6ac9992ebae2a64f94f1bc2a72b0722917
Laura <laura.gordillo.vargas@gmail.com>
2017-07-05 03:17:25 -0500

    Cambio url recibo de caja

--- app/static/AngularJS/RecibosCaja/recibosCajaRepository.js
+++ app/static/AngularJS/RecibosCaja/recibosCajaRepository.js
@@ -1,9 +1,11 @@
+var reciboCajaURL = global_settings.urlCORS + 'api/recibosCaja/';
 
 
 registrationModule.factory('recibosCajaRepository', function ($http) {
     return {
         pdfReciboCaja: function (tipo,folio,nodo) {
             return $http({
+                url: reciboCajaURL + 'pdfReciboCaja/',
                 method: "GET",
                 params: {
                     tipo: tipo,
